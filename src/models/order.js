1fd5c13423abb0836f56f9b77bd9813fa654f5cb
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-17 10:16:33 +0530

    feat: integrate src/models/order.js

---
+++ src/models/order.js
@@ -0,0 +1,8 @@
+class Order {
+    constructor(id, customerId, products) {
+        this.id = id;
+        this.customerId = customerId;
+        this.products = products;
+    }
+}
+module.exports = Order;
